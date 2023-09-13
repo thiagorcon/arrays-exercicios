87b71344d919cf56a2c02be11ab06e1d811e66f2
Thiago Rosa <thiagorcon@hotmail.com>
2023-09-13 13:25:16 -0300

    envio exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+// # Exercício 1
+
+// No primeiro exercício vamos praticar a **criação de arrays**. Crie 3 arrays diferentes:
+
+// - O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+// - O segundo array deve possuir apenas **strings**;
+// - O terceiro array deve possuir **números**, **strings** e **booleanos**.
+
+// Imprima cada um dos arrays acima, utilizando `console.log()`
+
+let arrayNum = [10,20,30,40,50]
+let arrayString = ['javascript','java','php']
+let arraysAll = [100,'Thiago',true]
+console.log(arrayNum);
+console.log(arrayString);
+console.log(arraysAll);
